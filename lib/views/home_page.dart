4e408b61c2b332935ac7579797717004ff744092
Manula Pasan <89297752+Manula413@users.noreply.github.com>
2024-12-20 13:13:42 +0530

    update

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -9,9 +9,61 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Welcome, ${user.displayName}")),
-      body: Center(
-        child: Text("Email: ${user.email}"),
+      appBar: AppBar(
+        title: Text("Welcome, ${user.displayName}"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildUserInfo("Email", user.email),
+            SizedBox(height: 16),
+            _buildUserInfo("Employee Code", user.employeeCode),
+            SizedBox(height: 16),
+            _buildUserInfo("Company Code", user.companyCode),
+            SizedBox(height: 16),
+            _buildUserInfo("User Code", user.userCode),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserInfo(String title, String value) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 4,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "$title:",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black87,
+            ),
+          ),
+        ],
       ),
     );
   }
